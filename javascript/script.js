e3234d8688510a29d624b2e56d004a29d8e8814c
Jennifer Orrbon <jenniferorrbon@gmail.com>
2026-06-16 21:56:44 +0200

    Refactor hero and navbar styles for improved layout and responsiveness

--- javascript/script.js
+++ javascript/script.js
@@ -61,5 +61,5 @@ function mouseOver() {
 function mouseOut() {
     profileMessage.style.visibility = "hidden";
 }
-//---------------------------mousover mousout on persons img -------------------
+
 
