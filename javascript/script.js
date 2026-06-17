f4e9aa7a84aa9f299c7bc08251eb13bf48f48ee3
Jennifer Orrbon <jenniferorrbon@gmail.com>
2026-06-17 09:05:57 +0200

    Refactor form validation and improve input constraints; update CSS styles for better layout

--- javascript/script.js
+++ javascript/script.js
@@ -62,4 +62,12 @@ function mouseOut() {
     profileMessage.style.visibility = "hidden";
 }
 
+//------------------------------------------------------------------
+function validateForm() {
+    let x = document.forms["myForm"]["firstname"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
 
