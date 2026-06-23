e360ebdc0bbdec1eac43c1c5529df32a7b5da5ab
Jennifer Orrbon <jenniferorrbon@gmail.com>
2026-06-23 13:31:08 +0200

    Refactor settings and HTML structure; remove unused form validation function

--- javascript/script.js
+++ javascript/script.js
@@ -58,12 +58,4 @@ function mouseOut() {
     profileMessage.style.visibility = "hidden";
 }
 
-//------------------------------------------------------------------
-function validateForm() {
-    let x = document.forms["myForm"]["firstname"].value;
-    if (x == "") {
-        alert("Name must be filled out");
-        return false;
-    }
-}
 
